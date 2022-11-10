aeb05f94659d2d7374c90de950e8281949dbf6c0
Daniel Miros <dmcmiros@gmail.com>
2022-11-10 16:14:19 -0300

    placar dnv

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Pong</title>
 </head>
+
 <body>
-    <div id="campo">
+  <div id="campo">
     <div id="jogador"></div>
     <div id="inimigo"></div>
     <div id="bola"></div>
+    <div id="linha"></div>
+  </div>
+  <div id="placarStyle"><label id="placarJogador" for=""></label>
+    <div id="placarPlayers">
+      <h2>PLAYER GUEST</h2>
     </div>
-    <label id="placarJogador" for=""></label>
-</body>
-</html>
+    <div id="placarResultadoPlayer">
+      <h1>0</h1>
+    </div>
+    <div id="placarResultadoCpu">
+      <h1>0</h1>
+    </div>
+  </div>
